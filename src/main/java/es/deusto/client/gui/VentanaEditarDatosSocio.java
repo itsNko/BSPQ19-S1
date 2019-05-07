087a4ba5dc85a1f2467002b299f2390d7399c3c4
Alexander Moreno <alexander.moreno@opendeusto.es>
2019-05-07 20:44:00 +0200

    Mejora de aspecto de la VentanaEditarDatosSocio

--- src/main/java/es/deusto/client/gui/VentanaEditarDatosSocio.java
+++ src/main/java/es/deusto/client/gui/VentanaEditarDatosSocio.java
@@ -1,8 +1,6 @@
 package es.deusto.client.gui;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 import es.deusto.client.controllers.ControllerAlquiler;
 import es.deusto.client.controllers.ControllerEditarDatosSocio;
@@ -11,19 +9,25 @@ import es.deusto.server.dto.SocioDTO;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.rmi.RemoteException;
 import java.awt.event.ActionEvent;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+
 import javax.swing.JPasswordField;
 
 public class VentanaEditarDatosSocio extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
-	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_3;
@@ -33,21 +37,21 @@ public class VentanaEditarDatosSocio extends JFrame {
 	private boolean modificacionCorrecta = false;
 	private JPasswordField passwordField;
 
-	//	/**
-	//	 * Launch the application.
-	//	 */
-	//	public static void main(String[] args) {
-	//		EventQueue.invokeLater(new Runnable() {
-	//			public void run() {
-	//				try {
-	//					VentanaEditarDatosSocio frame = new VentanaEditarDatosSocio(null, null);
-	//					frame.setVisible(true);
-	//				} catch (Exception e) {
-	//					e.printStackTrace();
+	//		/**
+	//		 * Launch the application.
+	//		 */
+	//		public static void main(String[] args) {
+	//			EventQueue.invokeLater(new Runnable() {
+	//				public void run() {
+	//					try {
+	//						VentanaEditarDatosSocio frame = new VentanaEditarDatosSocio(null, null, null);
+	//						frame.setVisible(true);
+	//					} catch (Exception e) {
+	//						e.printStackTrace();
+	//					}
 	//				}
-	//			}
-	//		});
-	//	}
+	//			});
+	//		}
 
 	/**
 	 * Create the frame.
@@ -56,7 +60,7 @@ public class VentanaEditarDatosSocio extends JFrame {
 		ventanaQueMeLlama = ventanaAnterior;
 
 		SocioDTO iniciado = controllerAlquileres.selectSocio(nombreIniciado);
-		
+
 		try {
 			this.controllerEditarDatosSocio = new ControllerEditarDatosSocio();
 		} catch (RemoteException e) {
@@ -65,33 +69,40 @@ public class VentanaEditarDatosSocio extends JFrame {
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
-		setBounds(100, 100, 550, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		setBounds(100, 100, 960, 540);
+
+		final JLabel background;
+		ImageIcon img = new ImageIcon("."+File.separator+"src"+File.separator+"resources"+File.separator+"fondoEditarDatosSocio.png");
+		Image im = img.getImage();
+		im = getScaledImage(im, 960,540);
+		ImageIcon finalImg= new ImageIcon(im);
+		getContentPane().setLayout(null);
+		background = new JLabel("", finalImg, JLabel.CENTER);
+		background.setBounds(0,0,960, 518);
+		getContentPane().add(background);
+		background.setLayout(null);
 
 		JLabel lblPassword = new JLabel("Password:");
 		lblPassword.setBounds(94, 131, 62, 20);
-		contentPane.add(lblPassword);
+		background.add(lblPassword);
 
 		JLabel lblNewLabel = new JLabel("Nombre completo:");
 		lblNewLabel.setBounds(51, 70, 108, 14);
-		contentPane.add(lblNewLabel);
+		background.add(lblNewLabel);
 
 		textField = new JTextField();
 		textField.setBounds(166, 67, 143, 20);
-		contentPane.add(textField);
+		background.add(textField);
 		textField.setColumns(10);
 
 		textField_1 = new JTextField();
 		textField_1.setBounds(166, 100, 143, 20);
-		contentPane.add(textField_1);
+		background.add(textField_1);
 		textField_1.setColumns(10);
 
 		passwordField = new JPasswordField();
 		passwordField.setBounds(166, 131, 143, 20);
-		contentPane.add(passwordField);
+		background.add(passwordField);
 
 		JButton btnVolver = new JButton("VOLVER");
 		btnVolver.addActionListener(new ActionListener() {
@@ -101,7 +112,7 @@ public class VentanaEditarDatosSocio extends JFrame {
 			}
 		});
 		btnVolver.setBounds(33, 227, 89, 23);
-		contentPane.add(btnVolver);
+		background.add(btnVolver);
 
 		JButton btnNewButton = new JButton("GUARDAR");
 		btnNewButton.addActionListener(new ActionListener() {
@@ -178,51 +189,62 @@ public class VentanaEditarDatosSocio extends JFrame {
 			}
 		});
 		btnNewButton.setBounds(418, 227, 94, 23);
-		contentPane.add(btnNewButton);
+		background.add(btnNewButton);
 
 		JLabel lblModificacinDeDatos = new JLabel("MODIFICACIÓN DE DATOS DE USUARIO");
 		lblModificacinDeDatos.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblModificacinDeDatos.setHorizontalAlignment(SwingConstants.CENTER);
 		lblModificacinDeDatos.setBounds(118, 11, 333, 14);
-		contentPane.add(lblModificacinDeDatos);
+		background.add(lblModificacinDeDatos);
 
 		JLabel lblApellidos = new JLabel("Apellidos:");
 		lblApellidos.setBounds(94, 100, 62, 20);
-		contentPane.add(lblApellidos);
+		background.add(lblApellidos);
 
 		JLabel lblDireccin = new JLabel("Direccion:");
 		lblDireccin.setBounds(94, 162, 70, 14);
-		contentPane.add(lblDireccin);
+		background.add(lblDireccin);
 
 		textField_3 = new JTextField();
 		textField_3.setBounds(166, 159, 143, 20);
-		contentPane.add(textField_3);
+		background.add(textField_3);
 		textField_3.setColumns(10);
 
 		JLabel lblDatosAntiguos = new JLabel("DATOS ACTUALES:");
 		lblDatosAntiguos.setBounds(308, 45, 143, 14);
-		contentPane.add(lblDatosAntiguos);
+		background.add(lblDatosAntiguos);
 
 		JLabel lblNewLabel_1 = new JLabel(iniciado.getNombreCompleto());
 		lblNewLabel_1.setBounds(353, 70, 169, 14);
-		contentPane.add(lblNewLabel_1);
+		background.add(lblNewLabel_1);
 
 		JLabel lblNewLabel_2 = new JLabel(iniciado.getApellidos());
 		lblNewLabel_2.setBounds(353, 103, 169, 14);
-		contentPane.add(lblNewLabel_2);
+		background.add(lblNewLabel_2);
 
 		JLabel lblNewLabel_3 = new JLabel(iniciado.getPassword());
 		lblNewLabel_3.setBounds(353, 134, 169, 14);
-		contentPane.add(lblNewLabel_3);
+		background.add(lblNewLabel_3);
 
 		JLabel label = new JLabel(iniciado.getDireccion());
 		label.setBounds(353, 162, 169, 14);
-		contentPane.add(label);
+		background.add(label);
 	}
 
 	public boolean contieneLetrasYNumeros(String s) {
 		String n = ".*[0-9].*";
 		String l = ".*[A-Z].*";
 		return s.matches(n) && s.matches(l);
+	}
+
+	private Image getScaledImage(Image srcImg, int w, int h){
+		BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2 = resizedImg.createGraphics();
+
+		g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		g2.drawImage(srcImg, 0, 0, w, h, null);
+		g2.dispose();
+
+		return resizedImg;
 	}
 }
