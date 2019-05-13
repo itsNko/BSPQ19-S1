6e7234ec8307ccc3f61bf4b0361afe0376bf8fe7
Alexander Moreno <alexander.moreno@opendeusto.es>
2019-05-13 18:29:40 +0200

    Tests utilizando contiperf

--- src/test/java/es/deusto/client/data/SocioTest.java
+++ src/test/java/es/deusto/client/data/SocioTest.java
@@ -32,104 +32,141 @@ public class SocioTest {
 		s.setAlquileres(alquileres);
 	}
 	
-//	@Rule 
-//	public ContiPerfRule i = new ContiPerfRule();
-//	@Test 
-//    @PerfTest(invocations = 100, threads = 5)
-//	@Required(max = 1500, average = 1000, throughput = 5)
+	@Rule public ContiPerfRule rule = new ContiPerfRule();
+    public static junit.framework.Test suite() {
+        return new JUnit4TestAdapter(SocioTest.class);
+    }
+    
+    @Test
+    @PerfTest(invocations = 1000, threads = 20)
+    @Required(max = 50, average = 10)
 	public void testGetNombre() {
 		assertEquals("Lucas", s.getNombre());
 	}
 	
 	@Test
+    @PerfTest(invocations = 1000, threads = 20)
+	@Required(max = 50, average = 10)
 	public void testSetNombre() {
 		s.setNombre("Pablo");
 		assertEquals("Pablo", s.getNombre());
 	}
 	
 	@Test
+    @PerfTest(invocations = 1000, threads = 20)
+    @Required(max = 50, average = 10)
 	public void testGetPassword() {
 		assertEquals("12345678A", s.getPassword());
 	}
 
 	@Test
+    @PerfTest(invocations = 1000, threads = 20)
+    @Required(max = 50, average = 10)
 	public void testSetPassword() {
 		s.setPassword("87654321A");
 		assertEquals("87654321A", s.getPassword());
 	}
 	
 	@Test
+	@PerfTest(invocations = 1000, threads = 20)
+    @Required(max = 50, average = 10)
 	public void testGetNombreCompleto() {
 		assertEquals("Lucas", s.getNombreCompleto());
 	}
 	
 	@Test
+	@PerfTest(invocations = 1000, threads = 20)
+    @Required(max = 50, average = 10)
 	public void testSetNombreCompleto() {
 		s.setNombreCompleto("Lu");
 		assertEquals("Lu", s.getNombreCompleto());
 	}
 	
 	@Test
+	@PerfTest(invocations = 1000, threads = 20)
+    @Required(max = 50, average = 10)
 	public void testGetApellidos() {
 		assertEquals("Montolla Molinero", s.getApellidos());
 	}
 	
 	@Test
+	@PerfTest(invocations = 1000, threads = 20)
+    @Required(max = 50, average = 10)
 	public void testSetApellidos() {
 		s.setApellidos("Montolla");
 		assertEquals("Montolla", s.getApellidos());
 	}
 	
 	@Test
+	@PerfTest(invocations = 1000, threads = 20)
+    @Required(max = 50, average = 10)
 	public void testGetDireccion() {
 		assertEquals("Aveninda Miranda Num.1 3C", s.getDireccion());
 	}
 	
 	@Test
+	@PerfTest(invocations = 1000, threads = 20)
+    @Required(max = 50, average = 10)
 	public void testSetDireccion() {
 		s.setDireccion("Nueva direccion");
 		assertEquals("Nueva direccion", s.getDireccion());
 	}
 	
 	@Test
+	@PerfTest(invocations = 1000, threads = 20)
+    @Required(max = 50, average = 10)
 	public void testGetMonedero() {
 		assertEquals(7.5, s.getMonedero(), 0);
 	}
 	
 	@Test
+	@PerfTest(invocations = 1000, threads = 20)
+    @Required(max = 50, average = 10)
 	public void testSetMonedero() {
 		s.setMonedero(10.75);
 		assertEquals(10.75, s.getMonedero(), 0);
 	}
 	
 	@Test
+	@PerfTest(invocations = 1000, threads = 20)
+    @Required(max = 50, average = 10)
 	public void testGetImagen() {
 		assertEquals("imagePrueba.jpg", s.getImagen());
 	}
 	
 	@Test
+	@PerfTest(invocations = 1000, threads = 20)
+    @Required(max = 50, average = 10)
 	public void testSetImagen() {
 		s.setImagen("nuevaImagen.png");
 		assertEquals("nuevaImagen.png", s.getImagen());
 	}
 
 	@Test
+	@PerfTest(invocations = 1000, threads = 20)
+    @Required(max = 50, average = 10)
 	public void testGetAlquileres() {
 		assertEquals(alquileres, s.getAlquileres());
 	}
 
 	@Test
+	@PerfTest(invocations = 1000, threads = 20)
+    @Required(max = 50, average = 10)
 	public void testSetAlquileres() {
 		s.setAlquileres(alquileres);
 		assertEquals(alquileres, s.getAlquileres());
 	}
 	
 	@Test
+	@PerfTest(invocations = 1000, threads = 20)
+    @Required(max = 50, average = 10)
 	public void testIsBloquearMaquina() {
 		assertFalse(s.isBloquearMaquina());
 	}
 	
 	@Test
+	@PerfTest(invocations = 1000, threads = 20)
+    @Required(max = 50, average = 10)
 	public void testSetBloquearMaquina() {
 		s.setBloquearMaquina(true);
 		assertTrue(s.isBloquearMaquina());
