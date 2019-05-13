9146e6ca27c31c9e908588dd11ce721089fd820a
Daniel Diez-Andino Lopez <diezandino.daniel@opendeusto.es>
2019-05-13 19:37:12 +0200

    Guia de prueba creada, ventana Guia modificada, Boton de la guia modificado

--- src/main/java/es/deusto/client/gui/VentanaGuia.java
+++ src/main/java/es/deusto/client/gui/VentanaGuia.java
@@ -1,14 +1,23 @@
 package es.deusto.client.gui;
 
+import java.awt.Graphics2D;
+import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
@@ -16,36 +25,63 @@ public class VentanaGuia extends JFrame {
 	
 	private JFrame MenuSocio;
 	
-	public VentanaGuia(final JFrame VentanaAnterior) {
+	private Image getScaledImage(Image srcImg, int w, int h){
+		BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2 = resizedImg.createGraphics();
+
+		g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		g2.drawImage(srcImg, 0, 0, w, h, null);
+		g2.dispose();
+
+		return resizedImg;
+	}
+	
+	public VentanaGuia(final JFrame VentanaAnterior) throws FileNotFoundException {
 		MenuSocio = VentanaAnterior;
 		
 		setTitle("Guia usuario");
 		setResizable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
 		setBounds(100, 100, 960, 540);
-		final JLabel background;
+		final JLabel background;	
+		ImageIcon img = new ImageIcon("."+File.separator+"src"+File.separator+"resources"+File.separator+"guia.png");
+		Image im = img.getImage();
+		im = getScaledImage(im, 960,540);
+		ImageIcon finalImg= new ImageIcon(im);
+		getContentPane().setLayout(null);
 		
-//		ImageIcon img = new ImageIcon("."+File.separator+"src"+File.separator+"resources"+File.separator+"articulosAAplicarDescuento.png");
-//		Image im = img.getImage();
-//		im = getScaledImage(im, 960,540);
-//		ImageIcon finalImg= new ImageIcon(im);
-//		getContentPane().setLayout(null);
-//		
-		background = new JLabel("", null, JLabel.CENTER);
+		background = new JLabel("", finalImg, JLabel.CENTER);
 		background.setBounds(0, 0, 960, 518);
 		getContentPane().add(background);
 		background.setLayout(null);
 		
-		
 		JTextArea areaTexto = new JTextArea(25,80);
 		areaTexto.setLineWrap(true);
 		areaTexto.setWrapStyleWord(true);
-
-		JScrollPane scroll = new JScrollPane(areaTexto);
+		areaTexto.setEditable(false);
+		
+		try {
+			FileReader f = new FileReader("."+File.separator+"src"+File.separator+"resources"+File.separator+"Prueba1.txt");
+			BufferedReader b = new BufferedReader(f);
+			areaTexto.setText(b.readLine());
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		JPanel panel = new JPanel(new GridLayout(0, 1));
+		
+		panel.add(areaTexto);
+		JScrollPane scroll = new JScrollPane(panel);
+		panel.setOpaque(false);
 		background.add(scroll);
+		scroll.setBounds(64, 60, 550, 360);
+		scroll.setOpaque(false);
+		scroll.getViewport().setOpaque(false);
+			
 		
 		
+		
+			
 		JButton btnVolver = new JButton("Volver");
 		btnVolver.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -60,4 +96,6 @@ public class VentanaGuia extends JFrame {
 //		btnVolver.setBorderPainted(false);
 		background.add(btnVolver);
 	}
+	
+	
 }
