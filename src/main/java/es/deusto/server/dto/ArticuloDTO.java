c962366222300b0386c5ca44cdd8635f3db37149
Daniel Diez-Andino Lopez <diezandino.daniel@opendeusto.es>
2019-05-16 18:21:13 +0200

    Comentarios en clases DTO

--- src/main/java/es/deusto/server/dto/ArticuloDTO.java
+++ src/main/java/es/deusto/server/dto/ArticuloDTO.java
@@ -2,6 +2,9 @@ package es.deusto.server.dto;
 
 import java.io.Serializable;
 
+/**
+ * Clase ArticuloDTO.
+ */
 public abstract class  ArticuloDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -11,6 +14,9 @@ public abstract class  ArticuloDTO implements Serializable {
 	private double precio;
 	private double descuento;
 	
+	/**
+	 * Constructor completo del objeto ArticuloDTO.
+	 */
 	public ArticuloDTO(String nombre, String caratula, double precio, double descuento) {
 		this.nombre = nombre;
 		this.caratula = caratula;
@@ -18,38 +24,69 @@ public abstract class  ArticuloDTO implements Serializable {
 		this.descuento = descuento;
 	}
 
+	/**
+	 * Devuelve el valor del atributo nombre del ArticuloDTO.
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * Actualiza el atributo nombre del ArticuloDTO con el valor que se introduce por parametro.
+	 * @param nombre
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * Devuelve el valor del atributo caratula del ArticuloDTO.
+	 */
 	public String getCaratula() {
 		return caratula;
 	}
 
+	/**
+	 * Actualiza el atributo caratula del ArticuloDTO con el valor que se introduce por parametro.
+	 * @param caratula
+	 */
 	public void setCaratula(String caratula) {
 		this.caratula = caratula;
 	}
 
+	/**
+	 * Devuelve el valor del atributo precio del ArticuloDTO.
+	 */
 	public double getPrecio() {
 		return precio;
 	}
 
+	/**
+	 * Actualiza el atributo precio del ArticuloDTO con el valor que se introduce por parametro.
+	 * @param precio
+	 */
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
 	
+	/**
+	 * Devuelve el valor del atributo descuento del ArticuloDTO.
+	 */
 	public double getDescuento() {
 		return descuento;
 	}
 
+	/**
+	 * Actualiza el atributo descuento del ArticuloDTO con el valor que se introduce por parametro.
+	 * @param descuento
+	 */
 	public void setDescuento(double descuento) {
 		this.descuento = descuento;
 	}
 	
+	/**
+	 * Devuelve el nombre de la clase ArticuloDTO.
+	 */
 	public abstract String getClassName();
 	
 }
--- src/main/java/es/deusto/server/dto/VideojuegoDTO.java
+++ src/main/java/es/deusto/server/dto/VideojuegoDTO.java
@@ -2,6 +2,9 @@ package es.deusto.server.dto;
 
 import java.io.Serializable;
 
+/**
+ * Clase VideojuegoDTO
+ */
 public class VideojuegoDTO extends ArticuloDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -11,6 +14,9 @@ public class VideojuegoDTO extends ArticuloDTO implements Serializable {
 	private String fecha_lan;
 	private double puntuacion;
 	
+	/**
+	 * Constructor completo del objeto VideojuegoDTO.
+	 */
 	public VideojuegoDTO(String nombre, double precio, String descripcion, String categoria, String fecha_lan, double puntuacion, String caratula, double descuento) {
 		super(nombre, caratula, precio, descuento);
 		this.descripcion = descripcion;
@@ -19,38 +25,69 @@ public class VideojuegoDTO extends ArticuloDTO implements Serializable {
 		this.puntuacion = puntuacion;
 	}
 
+	/**
+	 * Devuelve el valor del atributo descripcion del VideojuegoDTO.
+	 */
 	public String getDescripcion() {
 		return descripcion;
 	}
 
+	/**
+	 * Actualiza el atributo descripcion del VideojuegoDTO con el valor que se introduce por parametro.
+	 * @param descripcion
+	 */
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
 
+	/**
+	 * Devuelve el valor del atributo categoria del VideojuegoDTO.
+	 */
 	public String getCategoria() {
 		return categoria;
 	}
 
+	/**
+	 * Actualiza el atributo categoria del VideojuegoDTO con el valor que se introduce por parametro.
+	 * @param categoria
+	 */
 	public void setCategoria(String categoria) {
 		this.categoria = categoria;
 	}
 
+	/**
+	 * Devuelve el valor del atributo fecha_lan del VideojuegoDTO.
+	 */
 	public String getFecha_lan() {
 		return fecha_lan;
 	}
 
+	/**
+	 * Actualiza el atributo fecha_lan del VideojuegoDTO con el valor que se introduce por parametro.
+	 * @param fecha_lan
+	 */
 	public void setFecha_lan(String fecha_lan) {
 		this.fecha_lan = fecha_lan;
 	}
 
+	/**
+	 * Devuelve el valor del atributo puntuacion del VideojuegoDTO.
+	 */
 	public double getPuntuacion() {
 		return puntuacion;
 	}
 
+	/**
+	 * Actualiza el atributo puntuacion del VideojuegoDTO con el valor que se introduce por parametro.
+	 * @param puntuacion
+	 */
 	public void setPuntuacion(double puntuacion) {
 		this.puntuacion = puntuacion;
 	}
 
+	/**
+	 * Devuelve el nombre de la clase VideojuegoDTO.
+	 */
 	@Override
 	public String getClassName() {
 		return "VideojuegoDTO";
