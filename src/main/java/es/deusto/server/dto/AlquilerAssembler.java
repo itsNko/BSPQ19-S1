048d41fe0e4e1de551d330cbdadadcdbd7a77151
Eneko Pizarro <epizarro@lksnext.com>
2024-04-22 12:40:51 +0200

    Tried automatic tests

--- src/main/java/es/deusto/server/dto/AlquilerAssembler.java
+++ src/main/java/es/deusto/server/dto/AlquilerAssembler.java
@@ -26,31 +26,32 @@ public class AlquilerAssembler {
 
 		List<AlquilerDTO> alquileresDTO = new ArrayList<AlquilerDTO>();
 
-		if(!(alquileres.isEmpty() || alquileres == null)) {
+		if (alquileres != null) {
 			System.out.println("* Assembling Alquileres ...");
 
 			String[] partes;
 			for (Alquiler alq : alquileres) {
 				partes = alq.getNombreArticulo().split("-");
-				if(partes[1].equals("Pelicula")) {
+				if (partes[1].equals("Pelicula")) {
 					Pelicula t = (Pelicula) alq.getAlquilado();
 					PeliculaDTO pelDTO = pelAssem.assemble(t);
-					alquileresDTO.add(new AlquilerDTO(pelDTO, alq.getCoste(), alq.getFecha_inicio(), 
+					alquileresDTO.add(new AlquilerDTO(pelDTO, alq.getCoste(), alq.getFecha_inicio(),
 							alq.getFecha_fin(), alq.isEnCurso()));
-				} else if(partes[1].equals("Videojuego")) {
+				} else if (partes[1].equals("Videojuego")) {
 					Videojuego t = (Videojuego) alq.getAlquilado();
 					VideojuegoDTO videojuegoDTO = vidAssem.assemble(t);
-					alquileresDTO.add(new AlquilerDTO(videojuegoDTO, alq.getCoste(), alq.getFecha_inicio(), 
+					alquileresDTO.add(new AlquilerDTO(videojuegoDTO, alq.getCoste(), alq.getFecha_inicio(),
 							alq.getFecha_fin(), alq.isEnCurso()));
 				}
 
 			}
-			System.out.println("Después de Assembler alquileres");
-			System.out.println(alquileresDTO.get(0).getFecha_fin());
-			System.out.println(alquileresDTO.get(0).getAlquilado().getNombre());
-
+			/*
+			 * System.out.println("Después de Assembler alquileres");
+			 * System.out.println(alquileresDTO.get(0).getFecha_fin());
+			 * System.out.println(alquileresDTO.get(0).getAlquilado().getNombre());
+			 */
 		}
-		
+
 		return alquileresDTO;
 	}
 }
--- src/test/java/es/deusto/server/dto/AlquilerAssemblerTest.java
+++ src/test/java/es/deusto/server/dto/AlquilerAssemblerTest.java
@@ -1,85 +1,60 @@
-package es.deusto.server.dto;
-
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import es.deusto.client.data.Alquiler;
-import es.deusto.client.data.Articulo;
+import es.deusto.client.data.Pelicula;
+import es.deusto.server.dto.AlquilerAssembler;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+import es.deusto.client.data.Alquiler;
 import es.deusto.client.data.Pelicula;
 import es.deusto.client.data.Videojuego;
+import es.deusto.server.dto.AlquilerAssembler;
+import es.deusto.server.dto.AlquilerDTO;
+import es.deusto.server.dto.PeliculaAssembler;
+import es.deusto.server.dto.PeliculaDTO;
+import es.deusto.server.dto.VideojuegoAssembler;
+import es.deusto.server.dto.VideojuegoDTO;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class AlquilerAssemblerTest {
 
-	private AlquilerAssembler alqAssem;
-	private List<Alquiler> alquileres;
-	private Articulo a;
-	private String nombre = "nombre";
-	private double precio = 0;
-	private String sinopsis = "sinopsis";
-	private String genero = "genero";
-	private String fecha_estr = "fechaestr";
-	private double puntuacion = 0;
-	private String caratula = "caratula";
-	private double descuento = 0;
+    @Test
+    public void testAssemble() {
+        // Create sample Alquiler objects
+        Pelicula pelicula1 = new Pelicula("The Avengers");
+        Alquiler alquiler1 = new Alquiler(pelicula1, 10.0, "2022-01-01", "2022-01-07", true);
+        Alquiler alquiler2 = new Alquiler(new Videojuego("FIFA 22"), 15.0, "2022-02-01", "2022-02-07", false);
 
-	private Articulo b;
-	private String descripcion = "descripcion";
-	private String categoria = "categoria";
-	private String fecha_lan = "fecha_lan";
+        // Create a list of Alquiler objects
+        List<Alquiler> alquileres = Arrays.asList(alquiler1, alquiler2);
 
-	@Before
-	public void setUp() {
-		a = new Pelicula(nombre, precio, sinopsis, genero, fecha_estr, puntuacion, caratula, descuento);
-		b = new Videojuego(nombre, precio, descripcion, categoria, fecha_lan, puntuacion, caratula, descuento);
-		alqAssem = AlquilerAssembler.getInstance();
-	}
+        // Call the assemble method
+        List<AlquilerDTO> alquileresDTO = AlquilerAssembler.getInstance().assemble(alquileres);
 
-	@Test
-	public void constructorTest() {
-		assertNotNull(AlquilerAssembler.getInstance());
-	}
+        // Assert the size of the returned list
+        assertEquals(2, alquileresDTO.size());
 
-	@Test
-	public void AlquilerAssemblePeliculaTest() {
-		Alquiler alquiler = new Alquiler(a, 0, "1", "2", false, "algo-Pelicula");
-		alquileres = new ArrayList<Alquiler>();
-		alquileres.add(alquiler);
-		List<AlquilerDTO> alquileresDTO = alqAssem.assemble(alquileres);
+        // Assert the properties of the first AlquilerDTO object
+        AlquilerDTO alquilerDTO1 = alquileresDTO.get(0);
+        assertEquals("The Avengers", alquilerDTO1.getAlquilado().getNombre());
+        assertEquals(10.0, alquilerDTO1.getCoste(), 0.01);
+        assertEquals("2022-01-01", alquilerDTO1.getFecha_inicio());
+        assertEquals("2022-01-07", alquilerDTO1.getFecha_fin());
+        assertTrue(alquilerDTO1.isEnCurso());
 
-		boolean igual = false;
-
-		if(alquileres.get(0).getCoste() == alquileresDTO.get(0).getCoste() &&
-				alquileres.get(0).getFecha_fin().equals(alquileresDTO.get(0).getFecha_fin()) &&
-				alquileres.get(0).getFecha_inicio().equals(alquileresDTO.get(0).getFecha_inicio()) &&
-				alquileres.get(0).isEnCurso() == alquileresDTO.get(0).isEnCurso()) {
-			igual = true;
-		}
-
-		assertTrue(igual);
-	}
-
-	@Test
-	public void AlquilerAssembleVideoJuegoTest() {
-		Alquiler alquiler = new Alquiler(b, 0, "1", "2", false, "algo-Videojuego");
-		alquileres = new ArrayList<Alquiler>();
-		alquileres.add(alquiler);
-		List<AlquilerDTO> alquileresDTO = alqAssem.assemble(alquileres);
-
-		boolean igual = false;
-
-		if(alquileres.get(0).getCoste() == alquileresDTO.get(0).getCoste() &&
-				alquileres.get(0).getFecha_fin().equals(alquileresDTO.get(0).getFecha_fin()) &&
-				alquileres.get(0).getFecha_inicio().equals(alquileresDTO.get(0).getFecha_inicio()) &&
-				alquileres.get(0).isEnCurso() == alquileresDTO.get(0).isEnCurso()) {
-			igual = true;
-		}
-
-		assertTrue(igual);
-	}
+        // Assert the properties of the second AlquilerDTO object
+        AlquilerDTO alquilerDTO2 = alquileresDTO.get(1);
+        assertEquals("FIFA 22", alquilerDTO2.getAlquilado().getNombre());
+        assertEquals(15.0, alquilerDTO2.getCoste(), 0.01);
+        assertEquals("2022-02-01", alquilerDTO2.getFecha_inicio());
+        assertEquals("2022-02-07", alquilerDTO2.getFecha_fin());
+        assertFalse(alquilerDTO2.isEnCurso());
+    }
 }
